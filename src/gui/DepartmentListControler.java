0374ed1b599073ee7861d9581a9d86a45200d9c2
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-03-31 22:56:51 -0300

    Department form (dialog) design

--- src/gui/DepartmentListControler.java
+++ src/gui/DepartmentListControler.java
@@ -1,18 +1,27 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
@@ -36,8 +45,9 @@ public class DepartmentListControler implements Initializable {
 	private ObservableList<Department> obsList;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = Utils.currentStage(event);
+		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -67,6 +77,34 @@ public class DepartmentListControler implements Initializable {
 		obsList = FXCollections.observableArrayList(list);
 		tableviewDepartment.setItems(obsList);
 		}
+	
+	private void createDialogForm(String absolutName, Stage parentStage) {
+		
+		try {
+			var loader = new FXMLLoader(getClass().getResource(absolutName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter Department data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("Io Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+		
 	}
+	
+	
+	
+	
+	}
+
+
 
 
---
+++ src/gui/util/Utils.java
@@ -0,0 +1,17 @@
+package gui.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+	
+	// Verfica o stage de um evento
+	public static Stage currentStage (ActionEvent event) {
+		return (Stage) ((Node) event.getSource()).getScene().getWindow();
+		
+	}
+	
+	
+
+}
